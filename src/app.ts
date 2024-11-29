bd7e8079d705c68e1881d6edfa54bc008253dbfb
TinyDickLion <tinydicklionsden@gmail.com>
2024-11-29 05:46:28 -0500

    fix: app config

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use(helmet({
   crossOriginEmbedderPolicy: false, // Disable if you don't serve all resources over HTTPS or have cross-origin dependencies not serving COEP headers
 }));
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://aaa-test-env.vercel.app",
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
